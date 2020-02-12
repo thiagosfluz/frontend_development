10556b79a8a86cde945a595cab8219984fcb9ea5
thiagosfluz <thiagosfluz@gmail.com>
2020-02-11 23:25:13 -0300

    javascript

---
+++ html/JavaScript/main.js
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+console.error('thiai si an error');
+
+console.warn("this is a warning");
+
+/*let age = 30;
+
+age = 31;
+
+console.log(age);*/
+
+const name = "John";
+
+const age = 30;
+
+const isColl = true;
+
+const rating = 4.5;
+
+const x = null;
+
+const y = undefined;
+
+let z;
+
+console.log(age);
+
