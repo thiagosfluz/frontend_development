a6f093a0a85aa4edb862297d3a4215a4b4951497
thiagosfluz <thiagosfluz@gmail.com>
2020-02-15 09:26:04 -0300

    new

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -42,7 +42,7 @@ btn.style.background = 'red';*/
 
 
 
-const btn = document.querySelector('.btn');
+/*const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -54,4 +54,46 @@ btn.addEventListener('click', (e) => {
 
 
     
-});
+});*/
+
+
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myform.addEventListener('submit', onSubmit);
+
+
+function onSubmit(e){
+
+
+    e.preventDefault();
+    if(nameInput.value == '' || emailInput.value == ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() =>msg.remove(), 3000);
+    }
+    else{
+
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+
+        userList.appendChild(li);
+
+        // clear Fields
+
+        nameInput.value = '';
+        emailInput.value = '';
+        console.log('success');
+    }
+
+
+    }
+
+
+
+
